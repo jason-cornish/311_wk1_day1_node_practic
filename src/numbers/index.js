396500019b46731b45f64caf9e4bfe09477bb30c
Jason <raj.cornish5@gmail.com>
2021-05-09 18:19:27 -0500

    Add files via upload

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,15 +1,32 @@
 const isEven = (num) => {
-  // write code for numbers.isEven
+  if (num % 2 == 0) {
+    return true;
+  }
 
 }
 
 const sum = (arr) => {
-  // write code for numbers.sum
+  var sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 
 }
 
 const comboSum = (arr, sum) => {
-  // write code for numbers.comboSum
+  
+  for (let x = 0; x < arr.length; x++) {
+    for (let y = 0; y < arr.length; y++) {
+
+      if ( arr[x] + arr[y] == sum) {
+        return true;
+      } else {
+        return false;
+      }
+
+    }
+  }
 
 }
 
