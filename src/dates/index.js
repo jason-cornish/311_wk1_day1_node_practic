e458c18a0288bbecbe11733d7a03613c5735d94a
Jason <raj.cornish5@gmail.com>
2021-05-09 18:19:08 -0500

    Add files via upload

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,18 +1,27 @@
 // import moment here; use this package in each function
+let moment = require('moment')
 
 const today = () => {
   // write code for dates.today
-
+   moment().format('dddd');
+   let todayDay = moment().format('dddd');
+   console.log(todayDay)
+   return todayDay;
 }
 
 const calendar = () => {
   // write code for dates.calendar
+  
+  let calendarDate = moment().format('MMMM D[,] YYYY');
+  console.log(moment().format('MMMM D[,] YYYY'));
+  return calendarDate;
+  
 
 }
 
 const currentTime = () => {
-  // write code for dates.currentTime
-
+  let nowTime = moment().format('LTS');
+  return nowTime;
 }
 
 module.exports = {
