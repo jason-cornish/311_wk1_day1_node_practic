487bf96a61e6925c21a26240033ede78ce054507
Jason <raj.cornish5@gmail.com>
2021-05-09 18:19:46 -0500

    Add files via upload

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,26 @@
 const split = (str, delim) => {
-  // write code for strings.split
+  return str.split(delim);
 
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
+  let strArray = [];
+  for (let i = 0; i < str.length; i+=2) {
+    strArray.push(str.slice(i, i+2));
+  }
+  return strArray;
 
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
+  let strWords = str.split('');
+  strWords.reverse();
+  let strReversed = strWords.join('');
+  return strReversed;
 
+  
 }
 
 module.exports = {
